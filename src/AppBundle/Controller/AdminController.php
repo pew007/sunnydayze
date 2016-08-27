8b91ee207cb769cc3613e3605c11e50b24461b81
Pengyu Wang <bird0407@gmail.com>
2016-08-26 23:34:08 -0700

    project setup. add assets

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin_index")
+     */
+    public function indexAction()
+    {
+        return $this->render(':admin:index.html.twig');
+    }
+}
--- tests/Controller/AdminControllerTest.php
+++ tests/Controller/AdminControllerTest.php
@@ -4,15 +4,15 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class AdminControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/admin');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
