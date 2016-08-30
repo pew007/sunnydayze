d895873799ccbe638c769050396491f2e0d0bad0
Pengyu Wang <bird0407@gmail.com>
2016-08-29 17:15:50 -0700

    configure db migration

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,6 +16,30 @@ class AdminController extends Controller
      */
     public function indexAction()
     {
+        $date = date('Y-m-d H:i:s');
+        $user = $this->getCurrentUser();
+        $user->setLastLogin(new \DateTime($date));
+
+        $em = $this->getEntityManager();
+        $em->persist($user);
+        $em->flush();
+
         return $this->render(':admin:index.html.twig');
+    }
+
+    private function getEntityManager()
+    {
+        return $this->getDoctrine()
+                    ->getManager();
+    }
+
+    /**
+     * @return User
+     */
+    private function getCurrentUser()
+    {
+        return $this->get('security.token_storage')
+                    ->getToken()
+                    ->getUser();
     }
 }
